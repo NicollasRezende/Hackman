4071e109ae51e8128a9c943cc18863e13e257ecc
Carlos Fernandes <carlos.fernandes@seatecnologia.com.br>
2026-04-11 16:15:25 -0300

    feat: scaffold Spring Boot project with config, CORS, and H2

---
+++ backend/src/main/java/br/gov/df/guiacidadao/GuiaCidadaoApplication.java
@@ -0,0 +1,11 @@
+package br.gov.df.guiacidadao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaCidadaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GuiaCidadaoApplication.class, args);
+    }
+}
